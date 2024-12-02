c95fd716c91f9029d689025a9dd16ea520c25071
niamh <niamhfoley.dev@gmail.com>
2024-12-02 22:06:21 +0000

    update db host

--- config.py
+++ config.py
@@ -6,8 +6,9 @@ class Config:
     DB_USER = os.environ.get('DB_USER')
     DB_PASSWORD = os.environ.get('DB_PASSWORD')
     DB_NAME = os.environ.get('DB_NAME')
+    DB_HOST = os.environ.get('DB_HOST')
     SQLALCHEMY_DATABASE_URI = (
-        F"postgresql://{DB_USER}:{DB_PASSWORD}@localhost:5432/{DB_NAME}"
+        F"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:5432/{DB_NAME}"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     TEMPLATES_AUTO_RELOAD = True
