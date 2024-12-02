df2ac78f6a34168361e353a7aa33d9d90e635cea
niamh <niamhfoley.dev@gmail.com>
2024-12-02 23:04:59 +0000

    add default values for db for running local and render

--- config.py
+++ config.py
@@ -2,13 +2,13 @@ import os  # Importing the 'os' module to access environment variables.
 
 # Base configuration class for the application.
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')  # Secret key for session security, fetched from environment variables.
-    DB_USER = os.environ.get('DB_USER')  # Database username, fetched from environment variables.
-    DB_PASSWORD = os.environ.get('DB_PASSWORD')  # Database password, fetched from environment variables.
-    DB_NAME = os.environ.get('DB_NAME')  # Database name, fetched from environment variables.
-    DB_HOST = os.environ.get('DB_HOST')  # Database host (e.g., localhost or IP), fetched from environment variables.
+    SECRET_KEY = os.environ.get('SECRET_KEY', 'secret_key')  # Secret key for session security, fetched from environment variables.
+    DB_USER = os.environ.get('DB_USER', 'my_flask_user')  # Database username, fetched from environment variables.
+    DB_PASSWORD = os.environ.get('DB_PASSWORD', 'my_secure_password')  # Database password, fetched from environment variables.
+    DB_NAME = os.environ.get('DB_NAME', 'my_flask_app')  # Database name, fetched from environment variables.
+    DB_HOST = os.environ.get('DB_HOST', '0.0.0.0:5433')  # Database host (e.g., localhost or IP), fetched from environment variables.
     SQLALCHEMY_DATABASE_URI = (  # Connection string for SQLAlchemy to connect to the PostgreSQL database.
-        F"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:5432/{DB_NAME}"
+        F"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}/{DB_NAME}"
     )
     SQLALCHEMY_TRACK_MODIFICATIONS = False  # Disables SQLAlchemy's event system to save resources.
     TEMPLATES_AUTO_RELOAD = True  # Ensures templates are auto-reloaded during development.
